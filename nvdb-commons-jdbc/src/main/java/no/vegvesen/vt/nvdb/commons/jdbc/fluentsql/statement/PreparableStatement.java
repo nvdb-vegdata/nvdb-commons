c6e77c72bd381115a91c57a77a52cf06467f5a08
Tore Eide Andersen <tore.eide.andersen@kantega.no>
2019-10-14 13:33:00 +0200

    Allowing toString with specific dialect

--- nvdb-commons-jdbc/src/main/java/no/vegvesen/vt/nvdb/commons/jdbc/fluentsql/statement/PreparableStatement.java
+++ nvdb-commons-jdbc/src/main/java/no/vegvesen/vt/nvdb/commons/jdbc/fluentsql/statement/PreparableStatement.java
@@ -1,6 +1,7 @@
 package no.vegvesen.vt.nvdb.commons.jdbc.fluentsql.statement;
 
 import no.vegvesen.vt.nvdb.commons.jdbc.fluentsql.Context;
+import no.vegvesen.vt.nvdb.commons.jdbc.fluentsql.dialect.Dialect;
 import no.vegvesen.vt.nvdb.commons.jdbc.fluentsql.dialect.MySqlDialect;
 
 import java.util.List;
@@ -12,12 +13,16 @@ public abstract class PreparableStatement {
     abstract String sql(Context context);
     abstract List<Object> params();
 
-    @Override
-    public String toString() {
+    public String toString(Dialect dialect) {
         String stringifiedParams = params().stream()
                 .map(Objects::toString)
                 .collect(joining(", "));
 
-        return sql(Context.of(new MySqlDialect())) + (stringifiedParams.isEmpty() ? " with no params" : " with params " + stringifiedParams);
+        return sql(Context.of(dialect)) + (stringifiedParams.isEmpty() ? " with no params" : " with params " + stringifiedParams);
+    }
+
+    @Override
+    public String toString() {
+        return toString(new MySqlDialect());
     }
 }
